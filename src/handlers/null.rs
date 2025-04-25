a80efa27abe2422adf64af8819ea2cec992219b9
Jayasimha Raghavan <mr.jayasimha@gmail.com>
2025-04-24 22:00:04 -0700

    Tweaks to tests

--- src/handlers/null.rs
+++
@@ -1,8 +0,0 @@
-impl Handler for NullHandler {
-    fn handle(&mut self, record: &Record) -> bool {
-        if cfg!(debug_assertions) {
-            println!("NullHandler::handle: enabled = {}, level = {:?}, record level = {:?}", self.enabled(), self.level(), record.level());
-        }
-        self.enabled() && record.level() >= self.level()
-    }
-} 
